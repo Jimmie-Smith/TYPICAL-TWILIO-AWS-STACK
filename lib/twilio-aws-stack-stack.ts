20009ffa64f3e8a1b3569fe369dd242f17bc3320
Jimmie-Smith <countertenacity@gmail.com>
2022-08-27 18:27:34 -0500

    first commit

--- lib/twilio-aws-stack-stack.ts
+++ lib/twilio-aws-stack-stack.ts
@@ -6,6 +6,7 @@ import { join } from "path";
 import { CfnOutput } from "aws-cdk-lib";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as iam from "aws-cdk-lib/aws-iam";
+// import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class TwilioAwsStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -16,6 +17,33 @@ export class TwilioAwsStackStack extends cdk.Stack {
       handler: "connect-dynamo.handler",
       code: Code.fromAsset(join(__dirname, "./lambdas")),
     });
+
+    const smsHandler = new Function(this, "sms-connect-to-dynamo", {
+      runtime: Runtime.NODEJS_16_X,
+      memorySize: 512,
+      handler: "sms-connect-dynamo.handler",
+      code: Code.fromAsset(join(__dirname, "./lambdas")),
+    });
+    // const api = new apigateway.RestApi(this, 'callerInfoAPI');
+
+    // const dynamoLambdaIntegration = new apigateway.HttpIntegration('http://amazon.com');
+
+//Adding Our HTTP API Gateway Endpoint to Access DynamoDB
+
+// api.root.addMethod('ANY', dynamoLambdaIntegration);
+
+// const items = api.root.addResource('items');
+// items.addMethod('GET', dynamoLambdaIntegration);
+// items.addMethod('PUT', dynamoLambdaIntegration);
+
+// const callSid = items.addResource('{callSid}');
+// callSid.addMethod('GET', dynamoLambdaIntegration);
+// callSid.addMethod('DELETE', dynamoLambdaIntegration);
+
+// Then we add integrations to our Lambda at the top
+
+
+
 
     const listBucketPolicy = new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
@@ -29,7 +57,11 @@ export class TwilioAwsStackStack extends cdk.Stack {
       resources: ['*'],
     })
 
-    handler.role?.attachInlinePolicy(new iam.Policy(this, 'List s3 buckets and Lambda functions', {
+    handler.role?.attachInlinePolicy(new iam.Policy(this, 'Allow anything for s3 buckets and Lambda functions', {
+      statements: [listLambdasPolicy, listBucketPolicy]
+    }))
+
+    smsHandler.role?.attachInlinePolicy(new iam.Policy(this, 'Sms allow anything for s3 buckets and Lambda functions', {
       statements: [listLambdasPolicy, listBucketPolicy]
     }))
 
@@ -40,7 +72,7 @@ export class TwilioAwsStackStack extends cdk.Stack {
     console.log("Our DynamoDB Table Name: ", table.tableName);
 
     new CfnOutput(this, "Dynamo Table Name: ", { value: table.tableName });
-    new CfnOutput(this, "FirstApp", { value: handler.functionArn });
+    new CfnOutput(this, "Twilio Cloudformation Stack", { value: handler.functionArn });
 
   }
 }
