f21cde1d28c7e8226683779bf2ade6c06f9f2513
Jimmie-Smith <countertenacity@gmail.com>
2022-08-27 03:25:28 -0500

    Added base code for Twilio CDK stack-Saves infor to dynamo so far

--- lib/twilio-aws-stack-stack.ts
+++ lib/twilio-aws-stack-stack.ts
@@ -1,16 +1,46 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import { CfnOutput } from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class TwilioAwsStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const handler = new Function(this, "connect-to-dynamo", {
+      runtime: Runtime.NODEJS_16_X,
+      memorySize: 512,
+      handler: "connect-dynamo.handler",
+      code: Code.fromAsset(join(__dirname, "./lambdas")),
+    });
 
-    // The code that defines your stack goes here
+    const listBucketPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['s3:*'],
+      resources: ['*'],
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TwilioAwsStackQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const listLambdasPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['lambda:*'],
+      resources: ['*'],
+    })
+
+    handler.role?.attachInlinePolicy(new iam.Policy(this, 'List s3 buckets and Lambda functions', {
+      statements: [listLambdasPolicy, listBucketPolicy]
+    }))
+
+    const table = new dynamodb.Table(this, "Table", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+    });
+
+    console.log("Our DynamoDB Table Name: ", table.tableName);
+
+    new CfnOutput(this, "Dynamo Table Name: ", { value: table.tableName });
+    new CfnOutput(this, "FirstApp", { value: handler.functionArn });
+
   }
 }
